135b965e6cc9030a916b221c1a15a9505205a5f1
Leochukwu <chukwu17mc@gmail.com>
2024-04-03 00:44:08 -1100

    github Api users

---
+++ src/Cleanup.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react'
+
+const url = 'https://api.github.com/users'
+
+const Cleanup = () => {
+  const [users, setUsers] = useState([])
+
+  const getUsers = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    setUsers(users)
+    // console.log(users)
+  }
+
+  useEffect(() => {
+    getUsers()
+  }, [])
+
+  return (
+    <div className='main'>
+      <div className='section'>
+        <div className='head'>
+          <h1>fetch useEffect</h1>
+          <ul>
+            {users.map((user) => {
+              const { id, login, html_url, avatar_url } = user
+              return (
+                <li key={id}>
+                  <img src={avatar_url} alt={login} />
+                  <div className='details'>
+                    <h3>{login}</h3>
+                    <a href={html_url}>profile</a>
+                  </div>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+export default Cleanup
